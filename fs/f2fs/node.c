a8b7c2d141477f720d3ab9ee3ceb0e5ba7e0da8d
Changgyu Lee <changgyuu.lee@gmail.com>
2020-06-30 12:09:45 +0000

    atomic range read unlock bugfix

--- fs/f2fs/node.c
+++ fs/f2fs/node.c
@@ -3530,10 +3530,8 @@ void atomic_range_read_unlock(struct f2fs_inode_info *fi, unsigned long start, u
 	atomic_t* rlatomic = fi->rlatomic;
 	int i;
 	for (i = start; i <= last; i++) {
-		while(1) {
-			smp_mb__before_atomic();
-			atomic_dec(&rlatomic[i]);
-			smp_mb__after_atomic();
-		}
+		smp_mb__before_atomic();
+		atomic_dec(&rlatomic[i]);
+		smp_mb__after_atomic();
 	}
 }
